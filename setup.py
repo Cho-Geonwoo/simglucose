d2b2e6f1e3273b725a33309930ee528f230a91b6
unknown <joungju257@gmail.com>
2026-03-27 00:22:06 +0900

    chore: update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="simglucose",
-    version="0.3.1",
+    version="0.3.2",
     description="A Type-1 Diabetes Simulator as a Reinforcement Learning Environment in OpenAI gym or rllab (python implementation of UVa/Padova Simulator)",
     url="https://github.com/Cho-Geonwoo/simglucose",
     author="Geonwoo Cho",
